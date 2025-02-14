8669b173415fadc5374a8fd59e595ec54c7117e5
Augusto <augusto_g_dias@estudante.sesisenai.org.br>
2025-02-14 09:53:27 -0300

    criação da feature principal

--- __tests__/cubo.test.js
+++ __tests__/cubo.test.js
@@ -3,14 +3,14 @@ import Cubo from "../model/cubo.js"
 describe("testes da classe cubo", ()=>{
     test("teste da area total do cubo", ()=>{
         const aresta = 4
-        areaTotalEsperada = 6 * aresta * aresta
+        const areaTotalEsperada = 6 * aresta * aresta
         const cubo = new Cubo(aresta)
 
         expect(cubo.calcularAreaTotal()).toBeCloseTo(areaTotalEsperada,2)
     })
     test("teste do volume do cubo", ()=>{
         const aresta = 4
-        volumeEsperado = aresta * aresta * aresta
+        const volumeEsperado = aresta * aresta * aresta
         const cubo = new Cubo(aresta)
 
         expect(cubo.calcularVolume()).toBeCloseTo(volumeEsperado,2)
